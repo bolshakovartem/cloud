6a0bcbed944332308310789abc85c9481e3e7615
bolshakovartem <artemis4650@gmail.com>
2023-03-03 17:52:51 +0200

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Book> books = _dbContext.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if(book !=null)
+            {
+                _dbContext.Books.Add(book);
+                _dbContext.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int? Id)
+        {
+            ViewBag.BookId = Id ?? 0;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+
+            _dbContext.Purchases.Add(purchase);
+            _dbContext.SaveChanges();
+            return "Thank you, " + purchase.Person + ", for your purchase!";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Book b1 = new Book
+            {
+                Name = "Harry Potter",
+                Author = "J.K. Rowling",
+                Id = 1,
+                Price = 400
+            };
+            
+        }
+    }
+}
